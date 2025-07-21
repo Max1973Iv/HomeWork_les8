241926e6b5b9537a157713e00ad764f112986a5b
Max1973Iv <blgroup3758844@gmail.com>
2025-07-21 23:05:24 +0300

    Добавлены классы Customer и Product для представления клиентов и товаров в интернет-магазине; создан файл для системы интернет-магазина; добавлен скриншот.

---
+++ code_hw/clases/customer.py
@@ -0,0 +1,8 @@
+# класс customer представляет клиента с атрибутами name (имя) и orders (список заказов клиента)
+class Customer:
+    def __init__(self, name: str, orders: str):
+        self.name = name
+        self.orders= orders
+
+    def __str__(self):
+        return f"клиент (Имя={self.name}, Список заказов={self.orders})"
