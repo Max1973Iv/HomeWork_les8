97b6609a6f3528f9091a6fd3a0e10b4ed9d6daf0
Max1973Iv <blgroup3758844@gmail.com>
2025-07-22 23:42:46 +0300

    Обновлены комментарии в классах Discount и Order для улучшения описания методов и атрибутов.

--- code_hw/classes_1/discount.py
+++ code_hw/classes_1/discount.py
@@ -1,9 +1,19 @@
 """класс discount представляет описание скидок для заказов с атрибутами
-discount_percent (процент скидки) и description (описание скидки)"""
+discount_percent (процент скидки) и description (описание скидки)
+метод __init__ инициализирует объект с этими атрибутами
+метод __str__ возвращает строковое представление объекта для удобного вывода
+статический метод позволяет рассчитать цену со скидкой для различных видов скидок"""
 class Discount:
     def __init__(self, discount_percent, description):
         self.discount_percent = discount_percent  # процент скидки
         self.description = description  # описание скидки
-
+#
     def __str__(self):
         return f"Discount: {self.discount_percent}%, Description: {self.description}"  # строковое представление объекта
+#    @staticmethod
+    def calculate_discounted_price(original_price, discount_percent):
+        """Статический метод для расчета цены со скидкой"""
+        if discount_percent < 0 or discount_percent > 100:
+            raise ValueError("Discount percent must be between 0 and 100")
+        discounted_price = original_price * (1 - discount_percent / 100)
+        return round(discounted_price, 2)
--- code_hw/classes_1/order.py
+++ code_hw/classes_1/order.py
@@ -1,7 +1,8 @@
 """класс order представляет заказ с атрибутами products (список товаров в заказе)
 метод __init__ инициализирует объект с этими атрибутами
 метод __str__ возвращает строковое представление объекта для удобного вывода
-"""
+метод класса sum_total_orders возвращает общее количество заказов
+метод класса cash_total_orders возвращает общую стоимость всех заказов"""
 class Order:
     def __init__(self, products):
         self.products = products
@@ -9,4 +10,15 @@ class Order:
     def __str__(self):
         return f"Список {len(self.products)} товаров в заказе: {', '.join(str(product) for product in self.products)}"
     
-    
+#
+    @classmethod
+    def sum_total_orders(cls, orders):
+        """Метод класса для подсчета общего количества заказов"""
+        return len(orders)
+    @classmethod
+    def cash_total_orders(cls, orders):
+        """Метод класса для подсчета общей стоимости всех заказов"""
+        total = 0
+        for order in orders:
+            total += sum(product.price for product in order.products)
+        return total  
