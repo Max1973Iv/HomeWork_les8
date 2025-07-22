58bde0e14c8b410250099c6757ac277fd86b37fd
Max1973Iv <blgroup3758844@gmail.com>
2025-07-22 23:15:46 +0300

    Обновлены комментарии в классах Discount и Order для улучшения описания атрибутов и методов.

--- code_hw/classes_1/discount.py
+++ code_hw/classes_1/discount.py
@@ -1,3 +1,9 @@
-# класс discount представляет скидку для заказов
+"""класс discount представляет описание скидок для заказов с атрибутами
+discount_percent (процент скидки) и description (описание скидки)"""
 class Discount:
-    pass
+    def __init__(self, discount_percent, description):
+        self.discount_percent = discount_percent  # процент скидки
+        self.description = description  # описание скидки
+
+    def __str__(self):
+        return f"Discount: {self.discount_percent}%, Description: {self.description}"  # строковое представление объекта
